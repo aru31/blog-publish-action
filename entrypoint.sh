ba1b37ccbc8ef9fe7ab58e14e51f943bc3c46716
Sarthak Narayan <sarthak.narayang@gmail.com>
2022-03-28 13:22:58 +0530

    Adding variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,7 @@
 set -e
 
 echo "Hello from the bash world"
+echo "test variable -> ${INPUT_TEST_VARIABLE}"
+echo "Github Token  -> ${INPUT_GITHUB_TOKEN}"
+echo "Random Token  -> ${INPUT_RANDOM_TOKEN}"
 
