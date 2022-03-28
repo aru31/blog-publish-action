812d1e57432c59958bbd2839b5b39e2e1caf7758
aru31 <guptarpit1997@gmail.com>
2022-03-28 15:25:05 +0530

    Get Latest Blogs

--- get_blogs.py
+++ get_blogs.py
@@ -2,29 +2,15 @@ import requests
 import urllib
 
 
-r = requests.get("https://api.github.com/repos/aru31/test-blog-publish/git/trees/master?recursive=1")
-blogs = r.json()["tree"]
+# Assumption here is that user pushes a single blog at a given time via a commit
 
-blog_path = f"https://github.com/aru31/test-blog-publish/blob/master/blogs"
+all_commits = requests.get("https://api.github.com/repos/aru31/test-blog-publish/commits")
+latest_sha = all_commits.json()[0]["sha"]
 
-get_all_blogs = list()
+latest_commit = requests.get(f"https://api.github.com/repos/aru31/test-blog-publish/commits/{latest_sha}")
+latest_files = latest_commit.json()["files"]
 
-for blog in blogs:
-    if blog["path"].startswith("blogs/"):
-        get_all_blogs.append(blog["path"][6:])
-
-print(get_all_blogs)
-
-with open("bloglist.txt") as f:
-    all_blogs = f.read().splitlines()
-
-new_ones = list(set(get_all_blogs)^set(all_blogs))
-print(new_ones)
-
-with open("bloglist.txt", "a") as myfile:
-    for blog in new_ones:
-        myfile.write(f"{blog}\n")
-
-# Download files
-for blog in new_ones:
-    urllib.request.urlretrieve(f"https://raw.githubusercontent.com/aru31/test-blog-publish/master/blogs/{blog}", blog)
+for file in latest_files:
+    if (file["status"] == "added") or (file["status"] == "modified"):
+        get_file_name = file["filename"].split("/")
+        urllib.request.urlretrieve(file["raw_url"], get_file_name[-1])
