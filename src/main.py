8634b049a07236ded944f77dcafe257c369cf4f4
Sarthak Narayan <sarthak.narayang@gmail.com>
2022-03-29 11:58:01 +0530

    change name of downloaded file

--- src/main.py
+++ src/main.py
@@ -4,14 +4,17 @@ import urllib
 
 # Assumption here is that user pushes a single blog at a given time via a commit
 
-all_commits = requests.get("https://api.github.com/repos/aru31/test-blog-publish/commits")
+all_commits = requests.get(
+    "https://api.github.com/repos/aru31/test-blog-publish/commits")
 latest_sha = all_commits.json()[0]["sha"]
 
-latest_commit = requests.get(f"https://api.github.com/repos/aru31/test-blog-publish/commits/{latest_sha}")
+latest_commit = requests.get(
+    f"https://api.github.com/repos/aru31/test-blog-publish/commits/{latest_sha}")
 latest_files = latest_commit.json()["files"]
 
 for file in latest_files:
     if (file["status"] == "added") or (file["status"] == "modified"):
         get_file_name = file["filename"].split("/")
         if(get_file_name[-1].split(".")[-1] == "md"):
-            urllib.request.urlretrieve(file["raw_url"], get_file_name[-1])
+            #urllib.request.urlretrieve(file["raw_url"], get_file_name[-1])
+            urllib.request.urlretrieve(file["raw_url"], "blog.md")
